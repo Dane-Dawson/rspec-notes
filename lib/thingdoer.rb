2e6d4ccdcaad3bc36ad4b6f854adfbaf7258c7a8
Your Name <you@example.com>
2020-07-24 11:18:29 -0500

    thingdoer functions and tests

--- lib/thingdoer.rb
+++ lib/thingdoer.rb
@@ -1,16 +1,29 @@
 class Thingdoer
-    #The first test is set to see if the method equals "Hello"
+    #The first test is set to see if the method equals "Hello". This works because Ruby implicitly returns 
     def hello
         "Hello"
     end
 
-    #This test fails the previous test, because although the output is "Hello", the method itself does not equal "Hello". The test has been changed to pass by altering the expect().to into expect().not_to
+    #This second function has the same test written and also passes.
+    def return_hello
+        return "Hello"
+    end
+    
+    #One more example to show that the last string in the function is the one returned, the test for this function checks for "Goodbye".
+    def return_goodbye
+        "Hello"
+        "and"
+        "Goodbye"
+    end
+
+    #This test fails with the the previous testing parameter, because although it puts "Hello", the method itself does not return "Hello". The test has been changed to pass by altering the expect().to into expect().not_to
     def hello_fail
         puts "Hello"
     end
 
     #This test shows a way to use include testing. Use include to specify that a collection includes one or more expected objects. Note that even though there are two entries, include only tests if *any* object passes the condition
     def say_hello
-        message = ["Hello", "there"]
+        message = ["Hello", "and", "Goodbye"]
     end
+
 end
--- spec/thingdoer_spec.rb
+++ spec/thingdoer_spec.rb
@@ -7,6 +7,19 @@ RSpec.describe Thingdoer do
         end
     end
 
+    
+    describe '#return_hello' do
+        it "Method return_hello returns value of 'Hello'" do
+            expect(Thingdoer.new.return_hello).to eq("Hello")
+        end
+    end
+
+    describe '#return_goodbye' do
+        it 'Method return_goodbye should equal Hello' do
+            expect(Thingdoer.new.return_goodbye).to eq("Goodbye")
+        end
+    end
+
     describe '#hello_fail' do
         it 'Method hello_fail should *not* equal Hello' do
             expect(Thingdoer.new.hello_fail).not_to eq("Hello")
@@ -15,7 +28,8 @@ RSpec.describe Thingdoer do
 
     describe '#hello' do
         it 'Method say_hello should include Hello' do
-            expect(Thingdoer.new.say_hello[message]).to include("Hello")
+            expect(Thingdoer.new.say_hello).to include("Hello")
         end
     end
+
 end
